8a2f3e83f2af5ef9a43e92bde117d8fd38139117
Agressiv1njector Team <54889809+Agressiv1njector@users.noreply.github.com>
2020-04-15 05:31:24 +0700

    Update psqli.sh

--- psqli.sh
+++ psqli.sh
@@ -2134,7 +2134,7 @@ echo -e "\033[1m$N[$R+$N] \e[38;5;81m${O}PSQLI MENU\e[0m
 [${O}11${N}]. Dork generator
 [${O}12${N}]. New Admin Finder
 [${O}13${N}]. Psqli Sqli/Xss/LFI/AdminFinder Scanner
-[${G}ENTER${N}]\033[2m for exit${N}"
+\033[2m press Enter for exit${N}"
 echo -ne "[${R}?${N}] yg mana ?? "
 read d
 case $d in
